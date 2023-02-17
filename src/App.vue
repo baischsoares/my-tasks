47fb77585c124f45177cd1019b56db253fb7fe23
baischsoares <baischsoares@gmail.com>
2023-02-17 10:44:40 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> 
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+body{
+  margin: 0px;
+  padding: 0px;
+}
+ul{
+  list-style: none;
+}
+li, p, h1, h2, h3{
+  margin: 0px;
+  padding: 0px;
+}
+a{
+  text-decoration: none;
+}
+</style>
