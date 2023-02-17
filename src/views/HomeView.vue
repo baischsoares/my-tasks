3d3547d8b0300c4205d611a07302890bde27fb5f
baischsoares <baischsoares@gmail.com>
2023-02-17 12:01:20 -0300

    style: estilo modal geral

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ModalUsuario/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ModalUsuario from '@/components/ModalUsuario.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ModalUsuario
   }
 }
 </script>
