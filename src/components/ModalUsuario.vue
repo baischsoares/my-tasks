2d8984744f25732c70308c1563ec9626acfba968
baischsoares <baischsoares@gmail.com>
2023-02-17 12:20:53 -0300

    feat: adicionando ususario

--- src/components/ModalUsuario.vue
+++ src/components/ModalUsuario.vue
@@ -3,14 +3,14 @@
     <div class="modal-container">
       <button class="fechar">X</button>
       <label for="nome">Nome:
-        <input type="text" v-model="nome" required id="nome">
+        <input type="text" v-model="usuario.nome" required id="nome">
       </label>
       
       <label for="email">Email:
-        <input type="text" v-model="email" required id="email">
+        <input type="email" v-model="usuario.email" required id="email">
       </label>
-      
-      <button class="buttonModal">Criar usuário</button>
+      <p class="erro" v-if="erro"> Adcione seu nome e e-mail</p>
+      <button class="buttonModal" @click="adicionarUsuario">Criar usuário</button>
     </div>
   </div>
 </template>
@@ -21,79 +21,33 @@ export default {
   data(){
     return {
       usuario: {
-        nome: String,
-        email: String,
+        nome: '',
+        email: '',
+        tarefas: [],
+        tarefasFeitas: [],
+      },
+      erro: false
+    }
+  },
+  methods: {
+    adicionarUsuario(){
+      if(this.usuario.nome && this.usuario.email){
+        this.$store.dispatch('salvarUsuario', this.usuario);
+        this.erro = false
+      } else {
+        this.erro = true
       }
     }
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.modal{
-  position: absolute;
-  background:rgba(0,0,0,.1);
-  left: 0px;
-  top: 0px;
-  width: 100%;
-  height: 100%;
-  display: grid;
-  align-items: center;
-  justify-content: center;
-}
-.modal-container{
-  background: white;
-  display: grid;
-  gap: 25px;
-  padding: 25px 50px;
-  border-radius: 5px;
-  position: relative;
-  box-shadow: 20px 30px 60px rgba(0,0,0,.2);
-}
-.modal-container label{
-  color: var(--corTexto);
-}
-.modal-container input{
-  margin-top: 5px;
-  display: block;
-  border-radius: 5px;
-  border: 2px solid #dedede;
-  padding: 10px 5px;
-  color: var(--corTexto);
-  outline: none;
-}
-.modal-container input:focus{
-  border: 2px solid var(--corAzul);
-}
-.fechar{
-  position: absolute;
-  border-radius: 50%;
-  background: var(--corAlerta);
-  color: white;
-  border: none;
+<style>
+.erro{
+  color: var(--corAlerta);
+  font-size: 0.75rem;
   font-weight: 700;
-  padding: 9px 12px;
-  top: -15px;
-  right: -15px;
-  cursor: pointer;
-  box-shadow: -5px 5px 15px rgba(0,0,0,.2);
-}
-.fechar:hover{
-border: 1px solid var(--corTexto);
-color: var(--corTexto);
-}
-.buttonModal{
-  padding: 10px;
-  background: transparent;
-  border: 1px solid var(--corRoxa);
-  border-radius: 5px;
-  color: var(--corRoxa);
-  font-weight: 700;
-  cursor: pointer;
-}
-.buttonModal:hover{
-  background: var(--corRoxa);
-  color: white;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    usuario: {},
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    ADICIONAR_USUARIO(state, payload) {
+      state.usuario = payload;
+    },
   },
   actions: {
+    salvarUsuario(context, payload) {
+      context.commit('ADICIONAR_USUARIO', payload);
+      localStorage.setItem('usuario', JSON.stringify(this.state.usuario));
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
