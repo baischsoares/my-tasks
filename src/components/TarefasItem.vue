b750668a7fcdf410a077a7c7f9010a78fbdc3a0e
baischsoares <baischsoares@gmail.com>
2023-02-27 22:27:17 -0300

    feat: modal de informacoes das tarefas

--- src/components/TarefasItem.vue
+++ src/components/TarefasItem.vue
@@ -1,15 +1,31 @@
 <template>
  <div class="tarefa-item">
-  <span>{{ tarefa.prazo }}</span>
-  <span>{{ tarefa.titulo }}</span>
-  <div class="tags-tarefas">
-    <span v-for="(tag, index) in tarefa.tags" :key="index" :style="tag.estilo" class="tag"></span>
+
+      <span>{{ tarefa.prazo }}</span>
+      <span>{{ tarefa.titulo }}</span>
+
+      <div class="tags-tarefas">
+        <span v-for="(tag, index) in tarefa.tags" :key="index" :style="tag.estilo" class="tag"></span>
+      </div>
+
+      <div class="botoes">
+        <button  v-if="tarefa.descricao" class="infos-tarefa" @click="modalInformacao = true"> ? </button>
+        <button class="editar">Editar</button>
+        <button class="feita" @click="excluirTarefa(tarefa)">Feita</button>
+      </div>
+
+        <div v-if="modalInformacao" class="modal">
+          <div class="modal-container modal-infos-tarefa">
+            <button class="fechar" @click="modalInformacao = false">X</button>
+            <p class="titulo-tarefa">{{ tarefa.titulo }}</p>
+            <p class="prazo-tarefa"> {{ tarefa.prazo }}</p>
+            <p class="descricao-tarefa">Descrição:</p>
+            <div class="descricao-espaco">
+              <p class="descricao-tarefa"> {{ tarefa.descricao }}</p>
+            </div>
+          </div >
+        </div>
   </div>
-  <div class="botoes">
-    <button class="editar">Editar</button>
-    <button class="feita" @click="excluirTarefa(tarefa)">Feita</button>
-  </div>
- </div>
 </template>
 
 <script>
@@ -17,6 +33,11 @@
 export default {
   name: 'TarefaItem',
   props: ['tarefa'],
+  data(){
+    return{
+      modalInformacao: false,
+    }
+  },
   methods: {
     excluirTarefa(tarefa){
       let confirmacao = window.confirm('Tem certeza que quer excluir a tarefa?')
@@ -48,6 +69,7 @@ export default {
   gap: 15px;
 }
 .botoes button{
+  text-align: center;
   padding: 5px 10px;
   border-radius: 5px;
   font-size: 0.75rem;
@@ -70,5 +92,27 @@ export default {
   width: 15px;
   height: 5px;
   margin-left: 5px;
+}
+.modal-infos-tarefa{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+  gap: 15px;
+}
+.titulo-tarefa{
+  font-weight: 700;
+}
+.prazo-tarefa{
+  justify-self: flex-end;
+}
+.prazo-tarefa, .descricao-tarefa, .tag{
+  font-size: 0.75rem;
+}
+.descricao-espaco{
+  padding: 5px;
+  grid-column: 1/-1;
+  height: 200px;
+  border-radius: 4px;
+  border: 1px solid #ddd;
 }
 </style>
